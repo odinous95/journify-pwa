e9e52904cd43e02ebee2c92719050a3d64f5ca88
odinous95 <mjd.thif@icloud.com>
2026-01-21 16:24:54 +0100

    feat: implement HybridRepository for managing journey steps with online/offline support

---
+++ src/app.features/journey/infrastructure/repositories/hybridRepository.ts
@@ -0,0 +1,30 @@
+import { isOnline } from "../onlineStatus";
+import { IJourneyRepository } from "../../types";
+import { OfflineRepository } from "./offlineRepository";
+import { OnlineRepository } from "./onlineRepository";
+
+export class HybridRepository implements IJourneyRepository {
+  constructor(
+    private offlineRepo: OfflineRepository,
+    private onlineRepo: OnlineRepository,
+    private isOnline: () => boolean,
+  ) {}
+
+  async getSteps() {
+    return this.isOnline()
+      ? this.onlineRepo.getSteps()
+      : this.offlineRepo.getSteps();
+  }
+}
+
+export function createHybridRepository() {
+  if (typeof window === "undefined") {
+    throw new Error("HybridRepository must be created in the browser");
+  }
+
+  return new HybridRepository(
+    new OfflineRepository(),
+    new OnlineRepository(),
+    isOnline,
+  );
+}
