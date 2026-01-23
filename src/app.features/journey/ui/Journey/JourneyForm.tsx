3acb12bbc1fcf547851bdc8f1cc179ef1c3ae9ec
odinous95 <mjd.thif@icloud.com>
2026-01-23 11:12:11 +0100

    feat: implement AddTodoForm component for adding new todos

---
+++ src/app.features/journey/ui/Journey/JourneyForm.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+import { addTodo } from "@/app/(home)/action";
+
+export function AddTodoForm() {
+    async function onSubmit(formData: FormData) {
+        await addTodo(formData.get("title") as string, "00000000-0000-0000-0000-000000000000");
+    }
+
+    return (
+        <form action={onSubmit}>
+            <input name="title" />
+
+            <button>Add</button>
+        </form>
+    );
+}
