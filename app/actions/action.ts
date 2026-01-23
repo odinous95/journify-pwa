0a93fa803649ef8e837a4d70d8dbace70bf74bb2
odinous95 <mjd.thif@icloud.com>
2026-01-23 21:04:43 +0100

    feat: update addTodo function to accept journeyId, title, and description, and adjust API endpoint

--- app/actions/action.ts
+++ app/actions/action.ts
@@ -1,14 +1,25 @@
 "use server";
 
-import { revalidatePath, revalidateTag, updateTag } from "next/cache";
+import { updateTag } from "next/cache";
 
-export async function addTodo(journeyName: string, userId: string) {
-  await fetch("http://localhost:5000/dailyjourney/create", {
+export async function addTodo(
+  journeyId: string,
+  title: string,
+  description: string = "",
+) {
+  const response = await fetch("http://localhost:5000/step/create", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({ userId, journeyName }),
+    body: JSON.stringify({
+      journeyId,
+      title,
+      description,
+    }),
   });
 
-  // revalidateTag("dailyjourney", "max");
+  if (!response.ok) {
+    throw new Error("Failed to create step");
+  }
+
   updateTag("dailyjourney");
 }
