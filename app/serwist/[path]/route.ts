ade832bc2b1b21ae33ae9f916a0105da87ce0634
odinous95 <mjd.thif@icloud.com>
2026-01-23 02:32:13 +0100

    refactor: clean up route.ts by commenting out unused code and improving revision determination

--- app/serwist/[path]/route.ts
+++ app/serwist/[path]/route.ts
@@ -1,17 +1,35 @@
-// app/serwist/[path]/route.ts
-export const dynamic = "force-dynamic";
+// // app/serwist/[path]/route.ts
+// export const dynamic = "force-dynamic";
 
+// import { createSerwistRoute } from "@serwist/turbopack";
+
+// // 🚫 NO spawnSync, NO git
+// const revision =
+//   process.env.NEXT_PUBLIC_BUILD_ID ??
+//   process.env.VERCEL_GIT_COMMIT_SHA ??
+//   crypto.randomUUID();
+
+// export const { dynamicParams, revalidate, generateStaticParams, GET } =
+//   createSerwistRoute({
+//     additionalPrecacheEntries: [{ url: "/~offline", revision }],
+//     swSrc: "app/sw.ts",
+//     nextConfig: {},
+//   });
+import { spawnSync } from "node:child_process";
 import { createSerwistRoute } from "@serwist/turbopack";
 
-// 🚫 NO spawnSync, NO git
+// Using `git rev-parse HEAD` might not the most efficient
+// way of determining a revision. You may prefer to use
+// the hashes of every extra file you precache.
 const revision =
-  process.env.NEXT_PUBLIC_BUILD_ID ??
-  process.env.VERCEL_GIT_COMMIT_SHA ??
+  spawnSync("git", ["rev-parse", "HEAD"], { encoding: "utf-8" }).stdout ??
   crypto.randomUUID();
 
-export const { dynamicParams, revalidate, generateStaticParams, GET } =
+export const { dynamic, dynamicParams, revalidate, generateStaticParams, GET } =
   createSerwistRoute({
     additionalPrecacheEntries: [{ url: "/~offline", revision }],
     swSrc: "app/sw.ts",
+    // Copy relevant Next.js configuration (assetPrefix,
+    // basePath, distDir) over if you've changed them.
     nextConfig: {},
   });
