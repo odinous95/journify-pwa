49e065e1e4d0b71290a3929d4c955f4c428d58f6
odinous95 <mjd.thif@icloud.com>
2026-01-28 18:46:46 +0100

    feat: integrate Auth0 for authentication and add middleware support

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,14 +1,15 @@
-// app/providers.tsx
 "use client";
+
 import { ReactNode } from "react";
 import { SerwistProvider } from "@serwist/turbopack/react";
+import { Auth0Provider } from "@auth0/nextjs-auth0/client";
 
 export default function Providers({ children }: { children: ReactNode }) {
   return (
-    <SerwistProvider
-      swUrl="/serwist/sw.js"
-    >
-      {children}
-    </SerwistProvider>
+    <Auth0Provider>
+      <SerwistProvider swUrl="/serwist/sw.js">
+        {children}
+      </SerwistProvider>
+    </Auth0Provider>
   );
 }
