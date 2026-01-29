b28a6860fac7b66ec0d44bbaec438d9a07de559f
odinous95 <mjd.thif@icloud.com>
2026-01-30 00:54:13 +0100

    feat: refactor journey feature to support access token handling in JourneyRepository

--- src/app.features/journey/instance.ts
+++ src/app.features/journey/instance.ts
@@ -1,14 +1,22 @@
-import { IJourneyService } from "./interfaces";
+import { IJourneyRepository, IJourneyService } from "./interfaces";
 import { createJourneyService } from "./service";
 import { JourneyRepository } from "./repository";
 
 export function createJourneyFeature(service: IJourneyService) {
   return {
     getDailyJourney: service.getDailyJourney,
+    addDailyStep: service.addDailyStep,
   };
 }
 
-const journeyRepository = new JourneyRepository();
-const journeyService: IJourneyService = createJourneyService(journeyRepository);
-export const journeyFeature = createJourneyFeature(journeyService);
+export function createJourneyFeatureWithToken(accessToken?: string) {
+  const journeyRepository: IJourneyRepository = new JourneyRepository(
+    accessToken,
+  );
+  const journeyService: IJourneyService =
+    createJourneyService(journeyRepository);
+  return createJourneyFeature(journeyService);
+}
+
+export const journeyFeature = createJourneyFeatureWithToken();
 export type JourneyFeature = ReturnType<typeof createJourneyFeature>;
