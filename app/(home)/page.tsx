0eeeb104b1834258a0dfc1391983685025fe0994
odinous95 <mjd.thif@icloud.com>
2026-01-30 01:19:12 +0100

    feat: simplify Page component by removing direct journey data handling and integrating DailyJourneyServer

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,29 +1,14 @@
+import { AddDailyStepForm } from "@/src/app.features/journey/ui/client/JourneyForm";
+import { DailyJourneyServer } from "@/src/app.features/journey/ui/server/DailyJourneyServer";
+
 export const revalidate = 3600;
 
-import { auth0 } from "@/src/lib/auth0";
-import { AddDailyStepForm } from "@/src/app.features/journey/ui/JourneyForm";
-import { createJourneyFeatureWithToken } from "@/src/app.features/journey";
-export default async function Page() {
-  const journeyFeature = createJourneyFeatureWithToken((await auth0.getAccessToken()).token);
-  const dailyJourney = await journeyFeature.getDailyJourney();
-  console.log("Daily Journey:", dailyJourney);
-
+export default function Page() {
   return (
     <>
-      <div>
-        <h1>{dailyJourney.title}</h1>
-        <p>Steps: {dailyJourney.stepCount}</p>
-        <AddDailyStepForm />
-        <ul>
-          {dailyJourney?.steps?.map(step => (
-            <li key={step.id}>
-              <strong>{step.title}</strong>
-              <p>{step.description}</p>
-            </li>
-          ))}
-        </ul>
-      </div>
+      <AddDailyStepForm />
+      <DailyJourneyServer />
     </>
-  );
+  )
 }
 
